99c49a881bc19ecf1b6ae8988b4298f3f9142d4d
Administrator <dev@leadingsoft.eu>
2021-11-09 09:28:16 +0200

    commit-01

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAi0t6KueK0cCbRgMxz0d0F52QZWRmS20o",
+    authDomain: "portfolio-projects-4b9da.firebaseapp.com",
+    projectId: "portfolio-projects-4b9da",
+    storageBucket: "portfolio-projects-4b9da.appspot.com",
+    messagingSenderId: "949907467161",
+    appId: "1:949907467161:web:0ec21b820d917719494108",
+    measurementId: "G-MJ4XG62QW4"
+  }
 };
 
 /*
